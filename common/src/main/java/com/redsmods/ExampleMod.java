d89834cd8cee094413107ce7fc8c73d30371e62e
FalseMSP <mark.philip21@gmail.com>
2025-07-26 13:58:08 -0400

    initial commit

---
+++ common/src/main/java/com/redsmods/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.redsmods;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "sound_physics_perfected";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
